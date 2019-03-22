d684aa4c61cf4e86b1bff6d649161a51cf3570b0
tblock007 <the.t.block@gmail.com>
2019-03-22 10:52:39 -0600

    adding comments for some coaching problems

--- py/iwannabe_alt.py
+++ py/iwannabe_alt.py
@@ -1,17 +1,37 @@
 from heapq import heappush, heappop, nsmallest
 
+# Read first line
 n, k = [int(w) for w in input().split()]
+
+# Create three lists that will be used as "heaps"
+# https://en.wikipedia.org/wiki/Binary_heap
 bestA, bestD, bestH = [], [], []
+
+# Read in each pokenom and store (stat, index) pairs 
+# in the corresponding heap.
+# Note that we store the NEGATIVE of the stat here.
+# This is a small trick used because the default heap 
+# is a "min-heap", so the smallest element is always on 
+# top.  If we store the negatives of the stat, then this 
+# actually gives the largest stat, as desired.
 for index in range(n):
     a, d, h = [int(w) for w in input().split()]
     heappush(bestA, (-1 * a, index))
     heappush(bestD, (-1 * d, index))
     heappush(bestH, (-1 * h, index))
 
+# After all (stat, index) pairs are stored in the heaps, 
+# we simply need to extract the top k from each heap.
+# Here, we will naively throw them into the list, without 
+# worrying about duplicates.
+# Remember that the heaps store (stat, index) pairs.  We 
+# only care about the index, so after popping off the pair, 
+# we only throw the index, located at [1], into the team list.
 team = []
 for _ in range(k):
     team.append(heappop(bestA)[1])
     team.append(heappop(bestD)[1])
     team.append(heappop(bestH)[1])
 
+# Converting team to a set here handles duplicates.
 print(len(set(team)))
--- py/throwns.py
+++ py/throwns.py
@@ -1,25 +1,49 @@
+# Performs the actual throw.  Updates variable curr 
+# with the new result of throwing it a distance j
+# The use of global here makes coding faster - we can 
+# access curr and n variables without directly passing them to 
+# the function (e.g., throw(j, n, curr)), but this should 
+# only be used in a contest environment (i.e., NOT in 
+# production code!).
+# Separating this out into a function is not really necessary 
+# since it is a pretty simple function, but it does help 
+# reason about the main loop if we can trust that we have a 
+# function that will simply handle the throw for us.
 def throw(j):
     global n, curr
     curr += j
     curr %= n
 
+# Read the input
 n, k = [int(w) for w in input().split()]
 tokens = [s for s in input().split()]
+
+# A list that will be used as a stack (last-in-first-out) structure
+# Every time a throw is made, it will be recorded (pushed onto) the stack.
+# Every time an undo is needed, we will look at the top of the stack to 
+# see how to undo it (and pop the value off the stack in the process).
 stack = []
+
+# Tracks current position; starts at child 0
 curr = 0
 
+# i will track the string we are on
 i = 0
 while i < len(tokens):
+    # in the case of undo, we need to pop the appropriate amount off 
+    # the stack, and undo each throw that we pop off
     if tokens[i] == 'undo':
         i += 1
         m = int(tokens[i])
         for _ in range(m):
-            diff = stack.pop()
-            throw(-1 * diff)
+            diff = stack.pop() # gets the most recent throw
+            throw(-1 * diff) # "undoes" it by throwing in the opposite direction
+    # in the simple case of a number, we need only execute the throw
     else:
         diff = int(tokens[i])
         throw(diff)
-        stack.append(diff)
+        stack.append(diff) # make sure that every throw that gets made is recorded on the stack
     i += 1
 
+# The process is complete, so we can simply print out the result.
 print(curr)
